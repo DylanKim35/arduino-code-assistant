a1ee0025890889559598261ad5ce9e31c3a8ad22
saswt <saswt@YMK>
2024-05-22 15:26:49 +0900

    fix, refactor: csproj 인식 오류 해결, RelayCommand 개선

--- ArduinoCodeAssistant/RelayCommand.cs
+++ ArduinoCodeAssistant/RelayCommand.cs
@@ -15,31 +15,31 @@ namespace ArduinoCodeAssistant
     public class RelayCommand<T> : ICommand
     {
         private readonly Action<T> _execute;
-        private readonly Predicate<T> _canExecute;
+        private readonly Predicate<T>? _canExecute;
 
         public RelayCommand(Action<T> execute) : this(execute, null)
         {
 
         }
 
-        public RelayCommand(Action<T> execute, Predicate<T> canExecute)
+        public RelayCommand(Action<T> execute, Predicate<T>? canExecute)
         {
             _execute = execute ?? throw new ArgumentNullException("execute can not null");
             _canExecute = canExecute;
         }
 
-        public bool CanExecute(object parameter)
-        {
-            return _canExecute == null ? true : _canExecute((T)parameter);
-        }
-
-        public event EventHandler CanExecuteChanged
+        public event EventHandler? CanExecuteChanged
         {
             add => CommandManager.RequerySuggested += value;
             remove => CommandManager.RequerySuggested -= value;
         }
 
-        public void Execute(object parameter)
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute == null ? true : _canExecute((T)parameter);
+        }
+
+        public void Execute(object? parameter)
         {
             _execute((T)parameter);
         }
