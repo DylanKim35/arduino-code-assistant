65112111831e66c5b9d1f7e3264418378f262192
alalalek <alalalek@naver.com>
2024-05-25 03:43:17 +0900

    ✨ Add ChatRequest model

---
+++ ArduinoCodeAssistant/Models/ChatRequest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArduinoCodeAssistant.Models
+{
+    public class ChatRequest : INotifyPropertyChanged
+    {
+        public string _status;
+        public string _message;
+
+
+        public string Status
+        {
+            get { return _status; }
+            set
+            {
+                _status = value;
+                OnPropertyChanged(nameof(Status));
+            }
+        }
+
+        public string Message
+        {
+            get { return _message; }
+            set
+            {
+                _message = value;
+                OnPropertyChanged(nameof(Message));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
